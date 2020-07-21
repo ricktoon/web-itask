73418211a8c47daa3471c928c4d061e2db3f99ef
Henrique Oliveira <henrique.fsa97@hotmail.com>
2020-07-21 16:33:27 -0300

    header complete

---
+++ src/components/Header/styles.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+width: 100%;
+height: 70px;
+background: #090909;
+border-bottom: 5px solid #FF7811;
+display:flex;
+
+`
+export const LeftSide = styled.div`
+width: 50%;
+height: 70px;
+display: flex;
+align-items: center;
+padding-left: 15px;
+
+  img{
+    width:150px;
+    height:50px;
+  }
+
+`
+export const RightSide = styled.div`
+width: 50%;
+height: 70px;
+display:flex;
+align-items: center;
+justify-content: flex-end;
+
+  a{
+    color:#fff;
+    font-weight:bold;
+    text-decoration: none;
+    margin: 0 10px;
+
+    &:hover{
+      color:#FF7811;
+    }
+  }  
+
+  #notification{
+    img{
+      width: 25px;
+      height: 30px;
+    }
+
+    span{
+      background:#fff;
+      color:#FF7811;
+      padding: 5px 8px;
+      border-radius: 50%;
+      position: relative;
+      top: -20px;
+      right: 10px;
+    }
+
+    &:hover{
+      opacity:0.5;
+    }
+  }
+
+  .divider::after{
+    content:"|";
+    margin: 0 10px;
+    color: #fff;
+  }
+
+`
---
+++ src/views/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as S from './styles';
+
+import Header from '../../components/Header';
+
+function Home() {
+  return (
+  <S.Container>
+  <Header/>
+  </S.Container>
+  )
+}
+
+export default Home;
