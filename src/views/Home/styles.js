0d781f7eb8b679af292b9f71a9e599de7d7bd06a
Henrique Oliveira <henrique.fsa97@hotmail.com>
2020-07-21 17:34:13 -0300

    :art: Filter task

--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -4,3 +4,18 @@ export const Container = styled.div`
 width: 100%;
 
 `
+
+export const FilterArea= styled.div`
+display: flex;
+width: 100%;
+flex-wrap: wrap;
+justify-content: space-around;
+margin: 30px 0px;
+
+  button{
+    background: none;
+    border: none;
+  }
+
+`
+
